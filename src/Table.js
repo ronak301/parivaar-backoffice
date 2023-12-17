d15e8c818ecf0e6d9d63d35f8c8ca452dffe4135
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-12-18 00:21:16 +0530

    story created for react table

---
+++ src/Table.js
@@ -0,0 +1,195 @@
+import React from 'react'
+import {useTable,useSortBy,usePagination} from "react-table";
+import styled from 'styled-components'
+const Table=({ columns, data }) => {
+    
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      page,
+      prepareRow,
+      nextPage,
+      previousPage,
+      canPreviousPage,
+      canNextPage,
+      state:{pageIndex},
+      pageCount,
+      gotoPage
+    } = useTable({ columns, data ,
+    initialState:{pageSize:8}
+    
+    },useSortBy,usePagination);
+
+
+  return (
+    <Styles>
+    <table {...getTableProps()}>
+    <thead>
+      {headerGroups.map(headerGroup => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map(column => (
+            <th {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render("Header")}
+            {column.isSorted&&(
+<span style={{marginLeft:"5px"}}>
+{
+!column.isSortedDesc?" ⬆️ ":" ⬇️ "
+}
+</span>
+)
+
+
+            }
+            
+            
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+    <tbody {...getTableBodyProps()}>
+      {page.map((row, i) => {
+        prepareRow(row);
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map(cell => {
+              return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+            })}
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
+<div className='ContainerButton'>
+<button disabled={pageIndex===0} onClick={()=>gotoPage(0)}>First</button>
+<button  disabled={!canPreviousPage} onClick={previousPage}>Prev</button>
+
+<span style={{fontFamily:"Arial"}}>
+{pageIndex+1} of {pageCount}
+</span>
+<button disabled={!canNextPage} onClick={nextPage}>Next</button>
+
+<button disabled={pageIndex>=pageCount-1} onClick={()=>gotoPage(pageCount-1)}>Last</button>
+
+
+</div>
+
+</Styles>
+  )
+}
+const Styles = styled.div`
+  overflow-x: auto;
+  margin: 20px;
+
+  table {
+    width: 100%;
+    border-spacing: 0;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+
+    th,
+    td {
+      padding: 12px;
+      padding-left: 15px;
+      text-align: left;
+      font-family: "Arial";
+      border-bottom: 1px solid #ddd;
+    }
+
+    th {
+      background-color: #f5f5f5;
+      font-family: "Arial";
+      color: black;
+      font-weight: 600;
+    }
+
+    tbody tr:hover {
+      background-color: #f5f5f5;
+    }
+  }
+
+  .ContainerButton {
+    margin: auto;
+    width: 27rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    padding: 1rem;
+  }
+
+  .ContainerButton button {
+    border: none;
+    padding: 0.5rem 1rem;
+    background-color: #0d6efd;
+    color: white;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+
+  .ContainerButton button:disabled {
+    cursor: not-allowed;
+    background-color: rgb(229, 229, 229);
+    color: rgba(0, 0, 0, 0.267);
+  }
+
+  @media only screen and (max-width:898px) {
+    .ContainerButton button {
+font-size:12px;
+}
+.ContainerButton span{
+font-size:12px;
+
+
+}
+table{
+  font-size:12px;
+}
+.ContainerButton{
+flex-direction:row;
+width:90%;
+gap:0.5rem;
+}
+
+
+
+
+
+  }
+  @media only screen and (max-width:400px) {
+ 
+.ContainerButton{
+flex-direction:column;
+
+}
+.ContainerButton button{
+font-size:10px;
+
+}
+.ContainerButton span{
+font-size:10px;
+}
+table{
+font-size:12px;
+
+}
+
+
+
+
+
+  }
+
+
+
+
+
+`;
+
+
+
+
+
+
+
+export default Table
