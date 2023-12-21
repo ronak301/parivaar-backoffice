aea96be7d49c31c6c949b289a845634b7935fbb1
Tapasvi48 <Tapasviarora2002@gmail.com>
2023-12-21 16:23:24 +0530

    form created and conifg added

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Form from '../components/Form/Form.tsx';
 import MainPanel from '../components/MainPanel(Dashboard)/MainPanel';
+import Profile from '../moduels/auth/components/Dashboard/Profile/Profile.js';
+import Fire from '../components/Fire.js';
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/dash" element={<MainPanel />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/fire" element={<Fire />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
